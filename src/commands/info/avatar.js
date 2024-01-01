e3693b2ebc6898083229e9e86ebaadef2290d820
Aidh Aadil <helloaidh610@gmail.com>
2024-01-01 15:21:37 +0530

    Display GIF hyperlink for animated avatars

--- src/commands/info/avatar.js
+++ src/commands/info/avatar.js
@@ -27,11 +27,32 @@ module.exports = {
             const targetUserID = interaction.options.get('user')?.value || interaction.member.id
             const targetUser = await interaction.guild.members.fetch(targetUserID)
 
-            function getDescription() {
+            // Checking the extension of the avatarURL to see if it is a gif
+            function getExtension() {
                 if (targetUser.user.avatarURL()) {
-                    return `[WEBP](${targetUser.user.avatarURL({size: 4096})}) | [JPG](${targetUser.user.avatarURL({size: 4096, extension: 'jpg'})}) | [PNG](${targetUser.user.avatarURL({size: 4096, extension: 'png'})})`
+                    // Split the URL by '/' to get the filename
+                    const parts = targetUser.user.avatarURL().split('/');
+                    // Get the last part which should be the filename
+                    const filename = parts[parts.length - 1]; 
+                    // Split the filename by '.' to get the extension
+                    const extensionParts = filename.split('.');
+                    // Get the last part which should be the extension
+                    const extension = extensionParts[extensionParts.length - 1]; 
+                    
+                    return extension
                 } else {
+                    return 
+                }
+            }
+
+            function getDescription() {
+                if (!targetUser.user.avatarURL()) {
                     return `\`This user doesn't have an avatar\``
+                }
+                if (getExtension() === 'gif') {
+                    return `[GIF](${targetUser.user.avatarURL({size: 4096})})`
+                } else {
+                    return `[WEBP](${targetUser.user.avatarURL({size: 4096})}) | [JPG](${targetUser.user.avatarURL({size: 4096, extension: 'jpg'})}) | [PNG](${targetUser.user.avatarURL({size: 4096, extension: 'png'})})`
                 }
             }
 
