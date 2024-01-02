5419311c2f358bb4b3562e6c0fb2194294e3deb9
Aidh Aadil <helloaidh610@gmail.com>
2024-01-02 17:02:21 +0530

    Fix a bug

--- src/deployCommands.js
+++ src/deployCommands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js')
-const { clientId } = require('./config.json')
+const { clientId } = require('../config.json')
 const fs = require('node:fs')
 const path = require('node:path')
 require('dotenv').config()
