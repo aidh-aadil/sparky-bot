f659c0381d9da6808bd51a58eea6405a37cd2455
Aidh Aadil <helloaidh610@gmail.com>
2024-01-08 17:03:45 +0530

    Add dictionary command

---
+++ src/commands/fun/dictionary.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { colors } = require('../../../config.json')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('dictionary')
+    .setDescription('Search a word in the dictionary')
+    .addStringOption((option) =>
+      option
+        .setName('word')
+        .setDescription('The word you want to search')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    try {
+      await interaction.deferReply()
+
+      const word = interaction.options.getString('word')
+
+      let data = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      )
+
+      if (data.statusText == 'Not Found') {
+        return interaction.reply({
+          content: 'That word does not exist',
+          ephemeral: true,
+        })
+      }
+
+      let info = await data.json()
+      let result = info[0]
+
+      let embedInfo = await result.meanings.map((data, index) => {
+        let definition = data.definitions[0].definition || 'No definition found'
+        let example = data.definitions[0].example || 'No example found'
+
+        return {
+          name:
+            data.partOfSpeech.charAt(0).toUpperCase() +
+            data.partOfSpeech.slice(1),
+          value: `\`\`\` Description: ${definition} \n Example: ${example} \`\`\``,
+        }
+      })
+
+      const embed = new EmbedBuilder()
+        .setColor(colors.purple)
+        .setTitle(`Definition of **${result.word}**`)
+        .addFields(embedInfo)
+
+      await interaction.editReply({ embeds: [embed] })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+}
